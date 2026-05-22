8c36bf711fa1b3129e5dd9db08f476a8c770d60a
OLAMIDE <lutholamide78@gmail.com>
2026-05-22 15:16:36 +0100

    addfile

--- vendorcity/settings.py
+++ vendorcity/settings.py
@@ -89,17 +89,11 @@ WSGI_APPLICATION = 'vendorcity.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
 from decouple import config
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
-    }
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 
