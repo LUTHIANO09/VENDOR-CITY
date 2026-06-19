e25cc08a3d777231c0e34f49543b6fde727600bb
OLAMIDE <lutholamide78@gmail.com>
2026-06-19 14:35:55 +0100

    Add Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vendorcity.wsgi:application
--- vendorcity/settings.py
+++ vendorcity/settings.py
@@ -95,7 +95,7 @@ import dj_database_url
 from decouple import config
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default': dj_database_url.config(default=config('OFFLINE_URL'))
 }
 
 
